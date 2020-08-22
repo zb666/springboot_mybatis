1fd4d9fe4aa56bbb9a2f5ce92fbadce6f7b03a5f
zb666 <1360799362@qq.com>
2020-08-22 11:26:54 +0800

    Initial commit

---
+++ src/main/java/com/itheima/controller/MyBatisController.java
@@ -0,0 +1,27 @@
+package com.itheima.controller;
+
+import com.itheima.domain.User;
+import com.itheima.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.List;
+
+@Controller
+public class MyBatisController {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @RequestMapping("/query")
+    @ResponseBody
+    public List<User> findAllUser(){
+        List<User> allUser = userMapper.queryUserList();
+        return allUser;
+    }
+
+
+
+}
+
---
+++ src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,50 @@
+package com.itheima.domain;
+
+public class User {
+
+    private Long id;
+    private String username;
+    private String birthday;
+    private String sex;
+    private String address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
